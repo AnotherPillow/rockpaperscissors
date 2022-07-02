23f46326fe7ef3cc910d1dd9283b246e51e0557c
AnotherPillow <85362273+AnotherPillow@users.noreply.github.com>
2022-07-02 10:26:11 +0100

    updates!!

--- app.py
+++ app.py
@@ -1,27 +1,30 @@
 import random
 import os
 from won import winner
+from logo import logo
 
-print("""
-#####################################################################################################################################################
-#   ██████╗  ██████╗  ██████╗██╗  ██╗    ██████╗  █████╗ ██████╗ ███████╗██████╗     ███████╗ ██████╗██╗███████╗███████╗ ██████╗ ██████╗ ███████╗   #
-#   ██╔══██╗██╔═══██╗██╔════╝██║ ██╔╝    ██╔══██╗██╔══██╗██╔══██╗██╔════╝██╔══██╗    ██╔════╝██╔════╝██║██╔════╝██╔════╝██╔═══██╗██╔══██╗██╔════╝   #
-#   ██████╔╝██║   ██║██║     █████╔╝     ██████╔╝███████║██████╔╝█████╗  ██████╔╝    ███████╗██║     ██║███████╗███████╗██║   ██║██████╔╝███████╗   #
-#   ██╔══██╗██║   ██║██║     ██╔═██╗     ██╔═══╝ ██╔══██║██╔═══╝ ██╔══╝  ██╔══██╗    ╚════██║██║     ██║╚════██║╚════██║██║   ██║██╔══██╗╚════██║   #
-#   ██║  ██║╚██████╔╝╚██████╗██║  ██╗    ██║     ██║  ██║██║     ███████╗██║  ██║    ███████║╚██████╗██║███████║███████║╚██████╔╝██║  ██║███████║   #
-#   ╚═╝  ╚═╝ ╚═════╝  ╚═════╝╚═╝  ╚═╝    ╚═╝     ╚═╝  ╚═╝╚═╝     ╚══════╝╚═╝  ╚═╝    ╚══════╝ ╚═════╝╚═╝╚══════╝╚══════╝ ╚═════╝ ╚═╝  ╚═╝╚══════╝   #
-#####################################################################################################################################################
-""")
+def main():
+    print("What is your move?: "); player_guess = input()
 
-print("What is your move?: "); player_guess = input()
+    if player_guess == "rock":
+        player_guess = "rock"
+    elif player_guess == "paper":
+        player_guess = "paper"
+    elif player_guess == "scissors":
+        player_guess = "scissors"
 
-if player_guess == "rock":
-    player_guess = "rock"
-elif player_guess == "paper":
-    player_guess = "paper"
-elif player_guess == "scissors":
-    player_guess = "scissors"
+    computer_guess = random.choice(["rock", "paper", "scissors"])
 
-computer_guess = random.choice(["rock", "paper", "scissors"])
+    print(winner(player_guess,computer_guess))
+    print("Would you like to play again? (y/n)")
+    if input() == "y":
+        main()
+    else:
+        print("Thanks for playing!")
+        os.system("clear")
+        print(logo())
+        exit()
 
-print(winner(player_guess,computer_guess))
+if __name__ == "__main__":
+    print(logo())
+    main()
